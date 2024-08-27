d98723458677dc466328d8240a1fe4d152e26b86
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-08-28 00:40:02 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package CustomStack;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomStack customStack = new CustomStack();
+        customStack.push(1);
+        customStack.push(2);
+        customStack.push(3);
+
+
+
+    }
+}
